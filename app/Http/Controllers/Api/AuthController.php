7c2320767916017f1f4023089fbb325b0580491a
fajarsuhanasaputra <fajarsuhanaaa@gmail.com>
2022-10-16 01:57:13 +0700

    auth update

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'data' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (! Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login success',
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return response()->json([
+            'message' => 'logout success'
+        ]);
+    }
+
+    // update user profile
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|unique:users,email,' . Auth::user()->id
+        ]);
+        if(Auth::check())
+        {
+            if($request->input('password'))
+            {
+                $hashed = Hash::make($request->input('password'));
+                $user = User::find(Auth::user()->id);
+                $user->name = $request->input('name');
+                $user->email = $request->input('email');
+                $user->password = $hashed;
+                $user->update();
+            }
+
+            $user = User::find(Auth::user()->id);
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $user->update();
+        }
+        return response()->json([
+            'message' => 'update success'
+        ]);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,28 +3,29 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/**
- * route "/register"
- * @method "POST"
- */
-Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-/**
- * route "/login"
- * @method "POST"
- */
-Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 
-/**
- * route "/user"
- * @method "GET"
- */
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-/**
- * route "/logout"
- * @method "POST"
- */
-Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+});
+
+//update user
+Route::middleware('auth:sanctum')->group(function () {
+    Route::put('/user', [\App\Http\Controllers\Api\AuthController::class, 'update']);
+});
