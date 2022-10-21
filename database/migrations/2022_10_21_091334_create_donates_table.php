8186c5cce9290c074fd6efac53c0031391e177a3
fajarsuhanasaputra <fajarsuhanaaa@gmail.com>
2022-10-21 09:28:17 +0700

    add migration (donate & rekening) and model (donate & rekening)

---
+++ database/migrations/2022_10_21_091334_create_donates_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donates', function (Blueprint $table) {
+            $table->id();
+            $table->enum('jenis_donasi', ['zakat', 'infak', 'sedekah', 'dll']);
+            $table->bigInteger('nominal');
+            $table->string('nama', 50);
+            $table->string('alamat', 100);
+            $table->unsignedBigInteger('rekening_id');
+            $table->index('rekening_id');
+            $table->foreign('rekening_id')->references('id')->on('rekenings')->onDelete('cascade');
+            $table->string('telepon', 15);
+            $table->string('email', 50);
+            $table->text('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donates');
+    }
+};
