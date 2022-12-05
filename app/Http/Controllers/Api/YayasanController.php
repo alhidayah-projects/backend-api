56e4da89e0fffad91b9d1713baec6f80e212e4c1
fajarsuhanasaputra <fajarsuhanaaa@gmail.com>
2022-12-05 19:37:40 +0700

    create function add  yayasan in controller

---
+++ app/Http/Controllers/Api/YayasanController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class YayasanController extends Controller
+{
+    // create a new yayasan
+    public function create(Request $request)
+    {
+        // only admin can create yayasan
+        if (auth()->user()->role != 'admin') {
+            return response()->json([
+                'message' => 'anda bukan admin untuk membuat yayasan',
+            ], 403);
+        }
+
+        // validate request
+        $request->validate([
+            'nama_yayasan' => 'required|string',
+            'akte_notaris' => 'required|string',
+            'kemenkumham' => 'required|string',
+            'npwp' => 'required|string',
+            'sk_kota' => 'required|string',
+            'sk_provinsi' => 'required|string',
+            'profil_yayasan' => 'required|string',
+        ]);
+
+        // create yayasan
+        $yayasan = Yayasan::create([
+            'nama_yayasan' => $request->nama_yayasan,
+            'akte_notaris' => $request->akte_notaris,
+            'kemenkumham' => $request->kemenkumham,
+            'npwp' => $request->npwp,
+            'sk_kota' => $request->sk_kota,
+            'sk_provinsi' => $request->sk_provinsi,
+            'profil_yayasan' => $request->profil_yayasan,
+        ]);
+
+        // return yayasan
+        return response()->json([
+            'message' => 'yayasan berhasil dibuat',
+            'yayasan' => $yayasan,
+        ], 201);
+    }
+}
