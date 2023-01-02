df3ba6ebe3bc6336b4d803270a22476792b3172c
fajarsuhanasaputra <fajarsuhanaaa@gmail.com>
2023-01-02 11:57:45 +0700

    change migration Yayasan type data visi & misi

--- database/migrations/2022_12_05_195101_create_yayasans_table.php
+++ database/migrations/2022_12_05_195101_create_yayasans_table.php
@@ -22,9 +22,9 @@ return new class extends Migration
             $table->string('sk_kota')->nullable();
             $table->string('sk_provinsi')->nullable();
             $table->longText('profil_yayasan')->nullable();
-            $table->string('moto')->nullable();
-            $table->string('visi')->nullable();
-            $table->string('misi')->nullable();
+            $table->longText('moto')->nullable();
+            $table->longText('visi')->nullable();
+            $table->longText('misi')->nullable();
             $table->longText('alamat')->nullable();
             $table->string('no_telp')->nullable();
             $table->string('email')->nullable();
